113a418b3491fbb9f42afb0eb2c818428b05211b
Sidratul Muntaha <31538687+DNAMcKnight@users.noreply.github.com>
2024-12-15 17:21:57 +0600

    updated plugins and stuff

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,9 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
